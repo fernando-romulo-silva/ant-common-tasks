829e9859b2ee51d25c810ae9864587158f9bcf19
Fernando <fernando.romulo.silva@gmail.com>
2020-12-18 10:16:39 -0300

    news

---
+++ src/test/java/org/allset/config/ant/MavenPackageSimpleTest.java
@@ -0,0 +1,22 @@
+package org.allset.config.ant;
+
+// http://ant.apache.org/manual/tutorial-writing-tasks.html#TestingTasks
+public class MavenPackageSimpleTest {
+
+    // @Rule
+    // public final BuildFileRule buildRule = new BuildFileRule();
+    //
+    //
+    // @Before
+    // public void setUp() {
+    // // initialize Ant
+    // buildRule.configureProject("build.xml");
+    // }
+    //
+    //
+    // @Test
+    // public void testNestedText() {
+    // buildRule.executeTarget("build-clean");
+    // Assert.assertEquals("success", buildRule.getLog());
+    // }
+}
